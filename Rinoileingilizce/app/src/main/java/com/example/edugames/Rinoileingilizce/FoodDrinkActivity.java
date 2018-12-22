630b679b99cd37722059b0a734f7d2d7f1d2bad8
edemir <edemirr@users.noreply.github.com>
2018-12-22 22:43:00 +0300

    food drink 4 ana intent oluşturuldu

--- Rinoileingilizce/app/src/main/java/com/example/edugames/Rinoileingilizce/FoodDrinkActivity.java
+++ Rinoileingilizce/app/src/main/java/com/example/edugames/Rinoileingilizce/FoodDrinkActivity.java
@@ -1,7 +1,11 @@
 package com.example.edugames.Rinoileingilizce;
 
+import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class FoodDrinkActivity extends AppCompatActivity {
 
@@ -9,5 +13,44 @@ public class FoodDrinkActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_foodanddrink);
+
+        ImageView foods = findViewById(R.id.fooddrinkfood);
+        ImageView drinks = findViewById(R.id.fooddrinkdrink);
+        ImageView vege = findViewById(R.id.fooddrinkvegetable);
+        ImageView fruit =  findViewById(R.id.fooddrinkfruit);
+        ImageView orta = findViewById(R.id.fooddrinkorta);
+
+
     }
+    public void intentfood(View view){
+
+        Intent ıntent = new Intent(this,food_food.class);
+        startActivity(ıntent);
+
+    }
+    public void intentdrink(View view){
+
+        Intent ıntent = new Intent(this,food_drink.class);
+        startActivity(ıntent);
+
+    }
+    public void intentfruit(View view){
+
+        Intent ıntent = new Intent(this,food_fruit.class);
+        startActivity(ıntent);
+
+    }
+    public void intentvege(View view){
+
+        Intent ıntent = new Intent(this,food_vegetable.class);
+        startActivity(ıntent);
+
+    }
+
+
+
+
+
+
+
 }
